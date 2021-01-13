b1190ee9d2175bd23eaa6526443dfb8cf4f0c291
merima98 <merimaceranic98@gmail.com>
2021-01-14 00:37:37 +0100

    Adding UI for Home page

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,7 +1,36 @@
 import React from "react";
+import styled from "styled-components";
+import { MapPin } from "react-feather";
 
+import Login from "../login/Login";
+import { BREAKPOINTS } from "../../constants";
+
+const Wrapper = styled.div`
+  display: block;
+  @media (min-width: ${BREAKPOINTS.SMALL_DEVICES}) {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    margin: 0 auto;
+    width: 80%;
+  }
+`;
+const StyledImage = styled.div`
+  display: none;
+  @media (min-width: ${BREAKPOINTS.SMALL_DEVICES}) {
+    padding-top: 6rem;
+    text-align: center;
+    display: block;
+  }
+`;
 function Home() {
-  return <div> Home</div>;
+  return (
+    <Wrapper>
+      <StyledImage>
+        <MapPin style={{ width: "10rem", height: "10rem", color: "#0095f6" }} />
+      </StyledImage>
+      <Login />
+    </Wrapper>
+  );
 }
 
 export default Home;
