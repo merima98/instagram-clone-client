a4e4d8388ad3863beb153599c6037c5caad6e3f3
merima98 <merimaceranic98@gmail.com>
2021-01-18 13:15:09 +0100

    Adding dark theme for Login page

--- src/features/login/Login.js
+++ src/features/login/Login.js
@@ -3,18 +3,21 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 const Wrapper = styled.div`
+  align-items: center;
   padding-top: 3rem;
+  justify-content: space-around;
   max-width: 350px;
   margin: 0 auto;
 `;
 
 const Title = styled.h1`
   text-align: center;
+  color: ${(props) => props.theme.colors.titleColor};
 `;
 
 const Container = styled.div`
-  border: 1px solid #dbdbdb;
-  background-color: #ffffff;
+  border: 1px solid ${(props) => props.theme.colors.borderColor};
+  background-color: ${(props) => props.theme.colors.backgroundColor};
   margin-bottom: 1rem;
 `;
 
@@ -27,39 +30,42 @@ const Form = styled.form`
 const Input = styled.input`
   width: 80%;
   margin-bottom: 0.25rem;
-  background-color: #fafafa;
-  padding: 8px;
+  background-color: ${(props) => props.theme.colors.backgroundColorInput};
+  font-size: 14px;
+  padding: 9px 0px 7px 8px;
   border-radius: 4px;
-  border: 0.5px solid #dbdbdb;
+  border: 0.5px solid ${(props) => props.theme.colors.colorInputBorder};
   outline: none;
+  color: ${(props) => props.theme.colors.colorInput};
 `;
 
 const Submit = styled.button`
   width: 80%;
   padding: 5px 9px;
-  background-color: #0095f6;
+  background-color: ${(props) => props.theme.colors.backgroundColorSubmit};
   border-radius: 4px;
   margin-bottom: 1rem;
   border: none;
-  color: #fff;
+  color: ${(props) => props.theme.colors.color};
   outline: none;
 `;
 
 const SideContainer = styled.div`
   padding: 12px;
   text-align: center;
-  border: 1px solid #dbdbdb;
-  background-color: #ffffff;
+  border: 1px solid ${(props) => props.theme.colors.borderColor};
+  background-color: ${(props) =>
+    props.theme.colors.sideBarContainerBackgroundColor};
 `;
 const Label = styled.span`
   margin: 0 auto;
+  color: ${(props) => props.theme.colors.titleColor};
 `;
 
 const StyledLink = styled(NavLink)`
   text-decoration: none;
-  color: #0095f6;
+  color: ${(props) => props.theme.colors.styledLinkColor};
 `;
-
 function Login() {
   return (
     <Wrapper>
